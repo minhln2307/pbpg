09f41e1893dc6b142a8d907da0e2b0072b2974da
manhau174 <manhau.174@gmail.com>
2017-10-16 09:05:00 +0700

    migration factory hoan chinh

--- database/migrations/2017_10_08_075213_create_companies_table.php
+++ database/migrations/2017_10_08_075213_create_companies_table.php
@@ -17,9 +17,9 @@ class CreateCompaniesTable extends Migration
             $table->increments('id');
             $table->string('name', 50);
             $table->string('address', 100)->nullable();
-            $table->text('about')nullable();
+            $table->text('about')->nullable();
             $table->integer('user_id');
-            $table->string('mobile',13)->nullable();
+            $table->string('mobile',20)->nullable();
             $table->string('email', 50)->unique();
 
             $table->timestamps();
